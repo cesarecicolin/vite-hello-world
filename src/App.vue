f79436308d4475fe7b75243154f45c2e704d877c
cesarecicolin <cece.caster@gmail.com>
2023-11-22 16:52:10 +0100

    add: del mio primo components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
-<script setup>
+<script>
+import AppTitle from './components/AppTitle.vue'
 
+export default {
+  components :{
+    AppTitle
+  },
+  data(){
+    return{
+
+    }
+  }
+}
 </script>
 
 <template>
  <h1>ciao</h1>
+ <AppTitle/>
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data(){
+        return {
+            
+        }
+    },
+}
+</script>
+
+<template>
+<h2>La mia prima app con Vite!</h2>
+</template>
+
+<style>
+
+</style>
